51f2c03c75485dc01130cb2d0c8a0376ddeef493
HKEDU <HKEDU@DESKTOP-BP30565>
2020-02-14 13:08:01 +0900

    13:07 클래스파일 생성

---
+++ sp4-test02/src/main/java/Spring/MemberDTO.java
@@ -0,0 +1,11 @@
+package Spring;
+
+import java.util.Date;
+
+public class MemberDTO {
+	private Long id;
+	private String email;
+	private String password;
+	private String name;
+	private Date registerdate;
+}
